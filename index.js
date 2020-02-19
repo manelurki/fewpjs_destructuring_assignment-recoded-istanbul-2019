6d1040971d9494b0e10ccd084642a2a0b7f2eb74
manel turki <menelturki@gmail.com>
2020-02-19 20:16:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const doggie = {
-  first: 'Buzz',
-  breed: 'Great Pyrenees',
-  fur_color: 'black and white',
-  activity_level: 'sloth-like',
-  favorite_food: 'hot_dogs'
+const farmAnimals = 'cow horse sheep pig chicken'
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+const muppet = {
+  name: 'Kermit',
+  color: 'green',
+  song: 'The Rainbow Connection',
+  job: 'Host of The Muppet Show',
+  partner: 'Miss Piggy'
 };
 
+const k_muppet = {
+  k_name: 'Kermit',
+  k_color: 'green',
+  k_album: {
+    the_muppet_movie: {
+      song_1: 'Rainbow Connection',
+      song_2: 'Moving Right Along',
+      song_3: 'Never Before, Never Again',
+      song_4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  k_job: 'Host of The Muppet Show',
+  k_partner: 'Miss Piggy'
+};
+const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(" ")
+
+const [Bessie, , Dolly, Babe, Little] = 'cow horse sheep pig chicken'.split(" ")
+
+const [black_and_white, , black, pink] = 'cow horse sheep pig chicken'.split(" ")
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors
 
 
+const [r, o, y, g, b, , v] = colors
+
+ 
+const [ , , , , , indg, ] = colors
+
+const { name, color, song, job, partner} = muppet
+
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { song_2, song_4} = k_muppet.k_album.the_muppet_movie
+const { k_color, k_job, k_partner } = k_muppet
