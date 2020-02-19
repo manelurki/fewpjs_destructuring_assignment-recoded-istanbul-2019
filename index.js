bd9a808a96bb7111e69f9b3e6d869bf67c8d1e35
manel turki <menelturki@gmail.com>
2020-02-19 20:16:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,13 +33,8 @@ const [black_and_white, , black, pink] = 'cow horse sheep pig chicken'.split(" "
 const [red, orange, yellow, green, blue, indigo, violet] = colors
 
 
-const [r, o, y, g, b, , v] = colors
-
- 
-const [ , , , , , indg, ] = colors
 
 const { name, color, song, job, partner} = muppet
 
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const { song_2, song_4} = k_muppet.k_album.the_muppet_movie
 const { k_color, k_job, k_partner } = k_muppet
